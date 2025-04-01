e0d3fe4befe2e501b704cb3e9015e9cd6893fd59
bearlyachemist <39535854+bearlyachemist@users.noreply.github.com>
2025-04-01 15:42:31 -0400

    Update lint.lua

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,12 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        json = { "jsonlint" },
+        python = { 'ruff' },
+        go = { 'golangco-lint' },
+        dockerfile = { "hadolint" },
+        terraform = { "tflint" },
+        
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
